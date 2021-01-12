18c6f9bf34fdb6ac465c05da93652ad8829997c3
github akito-10 <akt.fukuda@gmail.com>
2021-01-12 23:34:08 +0900

    雨のアニメーションを修正

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,13 +1,43 @@
 anime({
-  targets: ".demo .el",
-  translateY: 500,
-  keyframes: [],
-  duration: function () {
-    return anime.random(500, 2000);
-  },
+  targets: ".rain-1 .el",
+  keyframes: [
+    { translateY: 75 },
+    { translateY: 100 },
+    { translateY: 150 },
+    { translateY: 200 },
+    { translateY: 250 },
+    { translateY: 300 },
+    { translateY: 350 },
+    { translateY: 400 },
+    { translateY: 450 },
+    { translateY: 500 },
+  ],
+  duration: 200,
   delay: function () {
     return anime.random(0, 600);
   },
   direction: "easeIn",
-  loop: false,
+  loop: true,
+});
+
+anime({
+  targets: ".rain-2 .el",
+  keyframes: [
+    { translateY: 75 },
+    { translateY: 100 },
+    { translateY: 150 },
+    { translateY: 200 },
+    { translateY: 250 },
+    { translateY: 300 },
+    { translateY: 350 },
+    { translateY: 400 },
+    { translateY: 450 },
+    { translateY: 500 },
+  ],
+  duration: 200,
+  delay: function () {
+    return anime.random(300, 900);
+  },
+  direction: "easeIn",
+  loop: true,
 });
